ad7d9871046f5d16708a8882d86656c1e5535ab3
davidmontesdeoca-hue <david.montecardona@gmail.com>
2025-11-18 17:02:57 -0500

    added clutter suppression

--- test1.m
+++ test1.m
@@ -5,12 +5,12 @@ Fs = 20e6;
 x = fread(fid_x, 2*len, 'int8');
 x_complex = double(x(1:2:end)) + 1i*double(x(2:2:end));
 x_complex = x_complex - mean(x_complex);
-ts_x = timeseries(x_complex, (0:len-1)');
+%ts_x = timeseries(x_complex, (0:len-1)');
 
 y = fread(fid_y, 2*len, 'int8');
 y_complex = double(y(1:2:end)) + 1i*double(y(2:2:end));
 y_complex = y_complex - mean(y_complex);
-ts_y = timeseries(y_complex, (0:len-1)');
+%ts_y = timeseries(y_complex, (0:len-1)');
 
 [R, lags] = xcorr(x_complex);
 R_normalized = abs(R)/max(abs(R));
@@ -24,20 +24,37 @@ ylim([-60, 10])
 xlim([-100, 100])
 hold on
 
-[R, lags] = xcorr(x_complex, y_complex);
-R_normalized = abs(R)/max(abs(R));
+
+lms = dsp.LMSFilter('Length', 1024, 'StepSize', 0.1, 'Method', 'Normalized LMS');
+
+[x_complex_filt, err, wts] = lms(x_complex,y_complex);
+
+
+[R_before, lags_before] = xcorr(x_complex, y_complex);
+R_normalized_before = abs(R_before)/max(abs(R_before));
+
+[R_after, lags_after] = xcorr(err, y_complex);
+R_normalized_after = abs(R_after)/max(abs(R_after));
+
 
 figure(2)
-plot(lags/Fs*1e6, 10*log10(R_normalized));
+plot(lags/Fs*1e6, 10*log10(R_normalized_after), 'b', 'LineWidth', 1.5);
 title('Cross-Correlation of Signals x and y');
 xlabel('Lag (µs)');
 ylabel('Normalized Cross-Correlation (dB)');
+hold on;
+plot(lags/Fs*1e6, 10*log10(R_normalized_before), 'r', 'LineWidth', 1.5);
+legend('After', 'Before')
 ylim([-60, 10])
 xlim([-100, 100])
 
-%plot(ts_x.Time, ts_x.Data, 'b', 'LineWidth', 1.5);
+%figure(3)
+%plot(real(err(1:1000)), 'b', 'LineWidth', 1.5);
+%title('Error signal')
 %hold on;
-%plot(ts_y.Time, ts_y.Data, 'r', 'LineWidth', 1.5)
+%figure(4)
+%plot(real(y_complex(1:100)), 'r', 'LineWidth', 1.5)
+%title('time domain signal y')
 %legend('x = 63', 'y = 63');
 %grid on;
 fclose(fid_x);
